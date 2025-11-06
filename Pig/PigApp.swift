48cd499a46d70ea2295c16a10585e3ed0959b48a
Alex Artamonov <aartamonov@bsd220.org>
2025-11-06 11:20:35 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Alex Artamonov on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
