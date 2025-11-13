2c98f038a997471499a70c8487243cf923c877e7
Alex Artamonov <aartamonov@bsd220.org>
2025-11-13 10:55:35 -0600

    Added basic game mechanics

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -24,15 +24,23 @@ struct ContentView: View {
                     .rotationEffect(.degrees(rotation))
                     .rotation3DEffect(.degrees(rotation),axis: (x: 1, y: 1, z: 0))
                     .padding(50)
-                    
-                    
+                
+                
                 CustomText(text: "Turn Score: \(turnScore)")
                 HStack{
                     Button("Roll"){
-                        
+                        chooseRandom(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                     Button("Hold"){
+                        gameScore += turnScore
+                        endTurn()
+                        withAnimation(.easeInOut(duration: 1)) {
+                            rotation += 360
+                        }
                         
                     }
                     .buttonStyle(CustomButtonStyle())
@@ -43,6 +51,30 @@ struct ContentView: View {
         }
         
     }
+    func endTurn(){
+        turnScore = 0
+        randomValue = 0
+    }
+    
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
+        if times == 0 {
+            if randomValue == 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    endTurn()
+                }
+            }
+            else {
+                turnScore += randomValue
+            }
+        }
+    }
+    
 }
 struct CustomText: View {
     let text: String
@@ -62,6 +94,9 @@ struct CustomButtonStyle: ButtonStyle {
     }
 }
 
+
 #Preview {
     ContentView()
 }
+    
+
